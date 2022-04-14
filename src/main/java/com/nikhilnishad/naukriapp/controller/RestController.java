68b09b514fb8ba6c458507cc3d289ade66e553ef
Nikhil Kr. Nishad <nikhilnishad279@gmail.com>
2022-04-15 01:52:03 +0530

    bug fix for submit button
    some controllers added

--- src/main/java/com/nikhilnishad/naukriapp/controller/RestController.java
+++ src/main/java/com/nikhilnishad/naukriapp/controller/RestController.java
@@ -1,6 +1,7 @@
 package com.nikhilnishad.naukriapp.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
@@ -17,6 +18,17 @@ public class RestController {
 	public String startBot(@RequestBody UserCred userCred) {
 		service.startBotForUser(userCred);
 		return "Profile Updated";
+	}
+	
+	@GetMapping("/stop")
+	public String stopBot() {
+		System.exit(0);
+		return "Stop Instruction Executed";
+	}
+	
+	@GetMapping
+	public String home() {
+		return "server is up";
 	}
 
 }
--- src/main/java/com/nikhilnishad/naukriapp/service/SeleniumService.java
+++ src/main/java/com/nikhilnishad/naukriapp/service/SeleniumService.java
@@ -8,6 +8,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
@@ -18,6 +19,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.springframework.stereotype.Service;
 
 import com.nikhilnishad.naukriapp.model.UserCred;
@@ -27,9 +30,8 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 @Service
 public class SeleniumService {
 	
-	private WebDriver driver;
 	
-    public void setUp(){
+    public WebDriver setUp(){
         System.out.println("Getting Ready");
         WebDriverManager.chromedriver().setup();
         ChromeOptions options = new ChromeOptions();
@@ -38,14 +40,15 @@ public class SeleniumService {
         //options.addArguments("--headless");
         String userAgent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36";
         options.addArguments("user-agent="+userAgent);
-        driver = new ChromeDriver(options);
+        WebDriver driver = new ChromeDriver(options);
         driver.navigate().to("https://www.naukri.com/nlogin/login");
         //driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(150,TimeUnit.SECONDS);
         System.out.println("Bot Ready for Login");
+        return driver;
     }
 
-    public void userLogin(String username, String passwd)
+    public void userLogin(WebDriver driver, String username, String passwd)
     {
         System.out.println("Trying to login");
         try{
@@ -91,17 +94,17 @@ public class SeleniumService {
         System.out.println("Login Completed");
     }
 
-    public void updateUserProfile() throws InterruptedException {
+    public void updateUserProfile(WebDriver driver) throws InterruptedException {
         System.out.println("Updating Profile");
         driver.navigate().to("https://www.naukri.com/mnjuser/profile?id=&orgn=homepage");
-        driver.manage().timeouts().implicitlyWait(150,TimeUnit.SECONDS);
-        Thread.sleep(10000);
+        driver.manage().timeouts().implicitlyWait(350,TimeUnit.SECONDS);
+        Thread.sleep(1000);
         WebElement editButtonForHeadline = driver.findElement(By.xpath("/html/body/div[2]/div/div/span/div/div/div/div/div/div[2]/div[3]/div[3]/div/div/div/div[1]/span[2]"));
         editButtonForHeadline.click();
-        Thread.sleep(500);
+        Thread.sleep(1000);
         WebElement textBoxInput=driver.findElement(By.id("resumeHeadlineTxt"));
         String headlineText = textBoxInput.getText();
-        Thread.sleep(500);
+        Thread.sleep(1000);
         if (!headlineText.endsWith(".")){
             headlineText+=".";
         }
@@ -109,33 +112,43 @@ public class SeleniumService {
         textBoxInput.clear();
         Thread.sleep(200);
         textBoxInput.sendKeys(headlineText);
-        Thread.sleep(1500);
-        driver.findElement(By.xpath("/html/body/div[5]/div[7]/div[2]/form/div[3]/div/button")).click();
+        Thread.sleep(200);
+        //as unable to find below xpath on multiple driver instance
+        //new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/div[5]/div[4]/div[2]/form/div[3]/div/button"))).click();
+//        List<WebElement> listOfButtons = driver.findElements(By.xpath("/html/body/div[5]/div[7]/div[2]/form/div[3]/div/button"));
+//        System.out.println(listOfButtons);
+//        if(listOfButtons.size()==0) {
+//        	driver.findElement(By.xpath("/html/body/div[5]/div[4]/div[2]/form/div[3]/div/button")).click();
+//        }
+//        driver.findElement(By.xpath("/html/body/div[5]/div[7]/div[2]/form/div[3]/div/button")).click();
+        
+        textBoxInput.submit();
         SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
         Date date = new Date();
         System.out.println(String.format("Updated Profile at:" + formatter.format(date)).toString());
     }
 
-    public void tearDown() throws InterruptedException {
+    public void tearDown(WebDriver driver) throws InterruptedException {
         if (driver != null) {
-            Thread.sleep(2000);
+            //Thread.sleep(2000);
+        	driver.manage().timeouts().implicitlyWait(500,TimeUnit.SECONDS);
             driver.quit();
         }
     }
 
 	public void startBotForUser(UserCred userCred) {
-		setUp();
-		userLogin(userCred.getEmail(),userCred.getPassword());
+		WebDriver driver=setUp();
+		userLogin(driver,userCred.getEmail(),userCred.getPassword());
 		System.out.println("login completed");
 		try {
-			updateUserProfile();
+			updateUserProfile(driver);
 			
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 		finally {
 			try {
-				tearDown();
+				tearDown(driver);
 			} catch (InterruptedException e) {
 				e.printStackTrace();
 			}
