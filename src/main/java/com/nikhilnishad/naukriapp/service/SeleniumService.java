613732b23dc89b879f35972aa37e1ddec0e57f86
Nikhil Kr. Nishad <nikhilnishad279@gmail.com>
2022-04-15 02:12:55 +0530

    headless mode activated

--- src/main/java/com/nikhilnishad/naukriapp/service/SeleniumService.java
+++ src/main/java/com/nikhilnishad/naukriapp/service/SeleniumService.java
@@ -37,12 +37,12 @@ public class SeleniumService {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        //options.addArguments("--headless");
+        options.addArguments("--headless");
         String userAgent="Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.75 Safari/537.36";
         options.addArguments("user-agent="+userAgent);
         WebDriver driver = new ChromeDriver(options);
         driver.navigate().to("https://www.naukri.com/nlogin/login");
-        //driver.manage().window().maximize();
+        driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(150,TimeUnit.SECONDS);
         System.out.println("Bot Ready for Login");
         return driver;
